4bc7e22106c55adfe696e160ede559854bf38500
Mark Danks <mark.danks@thetrainline.com>
2017-01-26 11:44:36 +0000

    Initial Commit

---
+++ CollectionViewExperiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewExperiments
+//
+//  Created by Mark Danks on 26/01/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
