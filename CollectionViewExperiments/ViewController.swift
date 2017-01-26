1a5f9b0eb25b813389ae97e6f55d213b6d5b449b
Mark Danks <mark.danks@thetrainline.com>
2017-01-26 15:30:15 +0000

    Adds some example cells using autolayout
    As you scroll through the content, see that the headers flake out as they approach the top of the collectionview.
    
    Also, there’s unsatisfiable constraints warnings happening, and I don’t know where they’re coming from.

--- CollectionViewExperiments/ViewController.swift
+++ CollectionViewExperiments/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let layout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionHeadersPinToVisibleBounds = true
+        
+        collectionView?.contentInset = UIEdgeInsetsMake(0, 0, -39, 0)
+        collectionView?.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, -39, 0)
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        let layout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.estimatedItemSize = CGSize(width: collectionView?.bounds.size.width ?? 0, height: 50)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 55
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CustomCell
+        
+        cell.heightConstraint.constant = CGFloat(indexPath.row * 10 % 100) + 10
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        return collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "Header", for: indexPath)
+    }
 }
 
+class CustomCell : UICollectionViewCell {
+    let identifier = "CustomCell"
+    
+    @IBOutlet weak var rectangle: UIView!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        translatesAutoresizingMaskIntoConstraints = false
+        rectangle.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
